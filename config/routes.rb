6c8600fd2b46ba13b9978be717d7d61fb1eacd7e
SudoRudo <r.guerreronyc@gmail.com>
2020-06-25 21:24:33 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students
+  resources :school_classes
+
+  patch 'school_classes/:id', to: 'school_classes#update'
+  patch 'students/:id', to: 'students#update'
+  
 end
